7c97c8a11da60e3f92abfd1e3e69e36233def354
Uriel Aispuro <urielalexis64@gmail.com>
2021-06-08 21:42:24 -0600

    feat: :sparkles: add new messages

--- message/text/lang/es.js
+++ message/text/lang/es.js
@@ -5,6 +5,8 @@ exports.wait = "*Espere un momento...*";
 exports.waitUntilNextCall = "*Espera 5 segundos antes de volver a llamar al bot.*";
 exports.groupOnly = `*Este comando solo puede ser usado en grupos*`;
 exports.tooLongText = "*¡El texto es demasiado largo!*";
+exports.maxVideoSeconds = "*El video/GIF debe durar máximo 5 segundos.*";
+exports.sticketNotFound = "*Sticker no encontrado. Prueba con algo distinto.*";
 exports.wrongFormat = `¡Formato incorrecto! Revisa el manual de uso enviando el comando *${prefix}help*`;
 exports.online = "*En linea. ✅*";
 exports.onlyBotAdmin =
@@ -17,7 +19,7 @@ exports.invalidLink = `*Enlace inválido*.`;
 exports.menuNsfw =
 	"*Comandos NSFW disponibles*\n\n1. /randomHentai 👧 \n2. /randomBoobs 🍈🍈\n3. /randomPussy 🥟\n4. /randomAss 🍑\n5. /random4k 📺\n6. /randomFeet 👣\n7. /randomCum 💦\n8. /randomBj 😮\n9. /randomAnal 🕳\n10. /randomGoneWild 👩‍🦰\n11. /randomTentacle 🦑";
 exports.nsfwStatus = `El modo *NSFW* no está activado en este grupo.\nActívalo con *${prefix}nsfw enable*`;
-exports.nsfwEnabled = `¡El modo NSWF se activó con éxito en este grupo!\nEnvía *${prefix}nsfwMenu * para ver qué opciones tengo para ti. 😉`;
+exports.nsfwEnabled = `¡El modo NSWF se activó con éxito en este grupo!\nEnvía *${prefix}nsfwMenu* para ver qué opciones tengo para ti. 😉`;
 exports.nsfwDisabled = "*¡El modo NSFW se desactivó con éxito en este grupo!*";
 exports.welcomeEnabled = "Modo *Welcome* activado";
 exports.welcomeDisabled = "Modo *Welcome* desactivado";
@@ -35,8 +37,61 @@ exports.mergingPDF = `*Merging PDFs...*`;
 exports.bye = "*Adiós. 👋*";
 exports.savedSuggestion = "*Sugerecia guardada. 👍*";
 exports.clearedSuggestions = "*Sugerecias eliminadas. 🗑*";
+exports.availableQuoteTags =
+	"*Available tags*\n\n• business\n• faith\n• famous-quotes\n• education\n• friendship\n• future\n• happiness\n• history\n• inspirational\n• life\n• love\n• nature\n• politics\n• proverb\n• religion\n• science\n• success\n• technology\n• wisdom";
+/* ================================ FUNCTIONS ===================================== */
+exports.stats = () => {
+	const stats = JSON.parse(fs.readFileSync("database/bot/stats.json"));
+	return `*Estadísticas de 🤖 UriBOT 🤖*\n\n*Versión del bot:* ${stats.version}\n*Fecha de inicio:* ${stats.startDate}\n*Total de llamadas al bot:* ${stats.totalCalls}\n*Archivos enviados:* ${stats.filesSent}\n*Stickers creados:* ${stats.stickersCreated}`;
+};
+exports.maxCount = (count) => `*Máximo ${count}*.`;
+exports.minCount = (count) => `*Mínimo ${count}*.`;
+exports.downloading = (value) => `*Descargando ${value}...*`;
+exports.making = (value) => `*Creando ${value}...*`;
+exports.searching = (value) => `*Buscando ${value}...*`;
+exports.converting = (from, to) => `*Convirtiendo de ${from} a ${to}...*`;
+exports.invalidLanguage = (lan, availableLanguages) =>
+	`Idioma *${lan}* inválido.\n\nIdiomas disponibles:\n${availableLanguages
+		.map((lan) => `*• ${lan}*`)
+		.join("\n")}`;
+exports.redditPost = (data) =>
+	`*Title:* ${data.title}\n*Post link:* ${data.postLink}\n*Subreddit:* ${data.subreddit}\n*Up Votes:* ${data.ups}\n*Original URL:* ${data.url}`;
+exports.googleFormat = (data) =>
+	`\n\n*Title:* ${data.title}\n*Desc:* ${data.snippet}\n*Link:* ${data.link}`;
+exports.newsFormat = (news) =>
+	`*Título:* ${news.title}\n*Descripción:* ${news.description}\n*Fecha:* ${news.publishedAt}\n*Contenido:* ${news.content}\n*URL:* ${news.url}`;
+exports.quoteFormat = (quote) => `*_“${quote.content}”_*\n\n${quote.author}`;
+exports.igProfile = (data) =>
+	`*Nombre:* ${data.name || "_No hay nombre_"}\n\n*Biografía:*\n${
+		data.bio || "_No hay biografía_"
+	}\n\n*Seguidores:*\n${data.followers}\n\n*Siguiendo:*\n${data.following}\n\n*Posts:*\n${
+		data.posts
+	}`;
+exports.generalError = (error) => `*Error:* ${error}`;
+exports.printSuggestions = (suggs) => {
+	let suggestions = "";
+	suggs.map((sug) => {
+		suggestions += `*Sugerido por:* ${sug.username + " (" + sug.uid + ")"}\n*Fecha:* ${
+			sug.date
+		}\n*Descripción:* ${sug.desc}\n\n`;
+	});
+	return suggestions.trimEnd() || "*No hay sugerencias.* 🤔";
+};
+
 exports.changelog = `*📌 CHANGELOG 📌*
-v1.4.0   |   CAMBIAR
+
+v1.5.0   |   fecha
+*[NEW]* /randomNews
+*[NEW]* /redditw
+*[NEW]* /randomQuote
+*[NEW]* /memeCreator
+*[REMOVED]* /xvid
+*[ADDED]* More translated messages.
+*[FIX]* NSFW command not working properly.
+*[FIX]* WELCOME command not working properly.
+*[FIX]* Some grammar mistakes.
+
+v1.4.0   |   01/06/2021
 *[NEW]* /math
 *[NEW]* /imgToText
 *[NEW]* /doc2pdf
@@ -67,7 +122,7 @@ v1.3.0   |   28/05/2021
 *[NUEVO]* /randomFeet
 *[NUEVO]* /randomBj
 *[NUEVO]* /randomCum
-*[NUEVO]* /randomGoNUEVOild
+*[NUEVO]* /randomGoneWild
 *[NUEVO]* /stats
 *[NUEVO]* /rp
 *[CAMBIO]* /commands (/readme before)
@@ -88,35 +143,3 @@ v1.2.0   |   25/05/2021
 *[NUEVO]* /igStalk
 *[OTRO]* Translated some command answers
 *[OTRO]* Fix some bugs`;
-
-/* ================================ FUNCTIONS ===================================== */
-exports.stats = () => {
-	const stats = JSON.parse(fs.readFileSync("database/bot/stats.json"));
-	return `*Estadísticas de 🤖 UriBOT 🤖*\n\n*Versión del bot:* ${stats.version}\n*Fecha de inicio:* ${stats.startDate}\n*Total de llamadas al bot:* ${stats.totalCalls}\n*Archivos enviados:* ${stats.filesSent}\n*Stickers creados:* ${stats.stickersCreated}`;
-};
-exports.maxCount = (count) => `*Máximo ${count}*.`;
-exports.downloading = (value) => `*Descargando ${value}...*`;
-exports.making = (value) => `*Creando ${value}...*`;
-exports.searching = (value) => `*Buscando ${value}...*`;
-exports.converting = (from, to) => `*Convirtiendo de ${from} a ${to}...*`;
-exports.invalidLanguage = (lan, availableLanguages) =>
-	`Idioma *${lan}* inválido.\n\nIdiomas disponibles:\n${availableLanguages
-		.map((lan) => `*• ${lan}*`)
-		.join("\n")}`;
-exports.redditPost = (data) =>
-	`*Title:* ${data.title}\n*Post link:* ${data.postLink}\n*Subreddit:* ${data.subreddit}\n*Up Votes:* ${data.ups}\n*Original URL:* ${data.url}`;
-exports.igProfile = (data) =>
-	`*Nombre:* ${data.name}\n\n*Biografía:*\n${data.bio}\n\n*Seguidores:*\n${data.followers}\n\n*Siguiendo:*\n${data.following}\n\n*Posts:*\n${data.posts}` +
-	!data.private
-		? `\n\nÚltimas publicaciones:\n\n${data.lastPosts}`
-		: "";
-exports.generalError = (error) => `*Error:* ${error}`;
-exports.printSuggestions = (suggs) => {
-	let suggestions = "";
-	suggs.map((sug) => {
-		suggestions += `*Sugerido por:* ${sug.username + " (" + sug.uid + ")"}\n*Fecha:* ${
-			sug.date
-		}\n*Descripción:* ${sug.desc}\n\n`;
-	});
-	return suggestions.trimEnd() || "*No hay sugerencias.* 🤔";
-};
